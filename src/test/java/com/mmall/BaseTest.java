c9aa3dfdfe500395244738a7de62e8e444ce1562
ivwsngghd <ivwsngghd@163.com>
2019-10-25 00:48:43 +0800

    fix bugs

--- src/test/java/com/mmall/BaseTest.java
+++ src/test/java/com/mmall/BaseTest.java
@@ -5,6 +5,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:applicationContext-datasource.xml","classpath:applicationContext.xml"}) //声明junit ,spring配置文件的位置
+@ContextConfiguration({"classpath:WEB-INF/lib/applicationContext-datasource.xml", "classpath:WEB-INF/lib/applicationContext.xml"}) //声明junit ,spring配置文件的位置
 public class BaseTest {
 }
