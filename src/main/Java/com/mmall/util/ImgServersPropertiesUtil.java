6623e28ab9645f4164581f39696ea485cff1a28d
ivwsngghd <ivwsngghd@163.com>
2019-12-18 12:20:37 +0800

    做图片分布式存储的预处理

---
+++ src/main/Java/com/mmall/util/ImgServersPropertiesUtil.java
@@ -0,0 +1,50 @@
+package com.mmall.util;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+public class PropertiesUtil {
+
+    //org.slf4j.Logger
+    private static Logger logger = LoggerFactory.getLogger(PropertiesUtil.class);
+
+    //java.util.Properties;
+    private static Properties props;
+
+    //该代码块用于类加载的时候，自动进行初始化加载一次；
+    static {
+        String fileName = "mmall.properties";
+        props = new Properties();
+        try {
+            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
+        } catch (IOException e) {
+            logger.error("配置文件读取异常",e);
+        }
+    }
+
+    public static String getProperty(String key){
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            return null;
+        }
+        return value.trim();
+    }
+
+    public static String getProperty(String key,String defaultValue){
+
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            value = defaultValue;
+        }
+        return value.trim();
+    }
+
+
+
+
+}
